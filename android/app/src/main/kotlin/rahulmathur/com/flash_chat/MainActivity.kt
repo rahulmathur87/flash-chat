18ac511de67add07aa09bea2bf002a5925c8b79a
Rahul <rahulmathur87@gmail.com>
2026-01-21 13:57:01 +0530

    first commit

---
+++ android/app/src/main/kotlin/rahulmathur/com/flash_chat/MainActivity.kt
@@ -0,0 +1,5 @@
+package rahulmathur.com.flash_chat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flash_chat/screens/welcome_screen.dart';
+import 'package:flash_chat/screens/login_screen.dart';
+import 'package:flash_chat/screens/registration_screen.dart';
+import 'package:flash_chat/screens/chat_screen.dart';
+
+void main() => runApp(FlashChat());
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.dark,
+        textTheme: ThemeData.dark().textTheme.copyWith(
+          bodyMedium: TextStyle(color: Colors.white70),
+        ),
+      ),
+      home: WelcomeScreen(),
+    );
+  }
+}
